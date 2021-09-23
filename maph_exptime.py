80f6fb2e13670091f36b9462d605f6000c606465
Angel <angelr4a1@gmail.com>
2021-09-22 23:53:37 -0500

    Espacios de syntax mejorados

--- maph_exptime.py
+++ maph_exptime.py
@@ -1,6 +1,6 @@
 from astropy.io import fits
-from glob import os #para windows
-import glob #para linux
+from glob import os  # para windows
+import glob  # para linux
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -10,24 +10,23 @@ import matplotlib.pyplot as plt
 # Ordenar (por nombre) los archivos en una lista para posterior consulta
 pathToFiles = "../DATA"
 orderedList = sorted(glob.glob(os.path.join(pathToFiles, '*.fts')))
-contador=0
+contador = 0
 for filename in orderedList:
-    contador+=1
+    contador += 1
 print("\nExisten {} archivos fts".format(contador))
-
 
 #############################################################
 
 # Obtener lista de datos que son 2048x2048
 listaDatos = []
 for filename in orderedList:
-  hdulist = fits.open(filename)
-  data = hdulist[0].data
-  hdulist.close
-  if data.shape == (2048, 2048):
-    listaDatos.append(filename)
-  else:
-    break
+    hdulist = fits.open(filename)
+    data = hdulist[0].data
+    hdulist.close
+    if data.shape == (2048, 2048):
+        listaDatos.append(filename)
+    else:
+        break
 print("\nDe los cuales {} son  de tamaño 2048 x 2048".format(len(listaDatos)))
 
 #############################################################
@@ -36,10 +35,10 @@ print("\nDe los cuales {} son  de tamaño 2048 x 2048".format(len(listaDatos)))
 hdulist1 = fits.open(listaDatos[0])
 headerList = hdulist1[0].header
 hdulist1.close()
-maph=pd.DataFrame(columns=headerList)
+maph = pd.DataFrame(columns=headerList)
 
 # fill Dataframe
-cel =[]
+cel = []
 for i in range(40):
     hdulist1 = fits.open(listaDatos[i])
     headerList = hdulist1[0].header
@@ -47,7 +46,7 @@ for i in range(40):
     for headerInplace in headerList:
         cel.append(headerList[headerInplace])
     maph.loc[i] = cel
-    cel=[]
+    cel = []
 
 #############################################################
 
@@ -55,10 +54,9 @@ for i in range(40):
 tama = len(listaDatos)
 expTime = maph.EXPTIME
 mExpTime = np.mean(expTime)
-maph_tem = maph[maph.EXPTIME>=mExpTime-1.5][maph.EXPTIME<=mExpTime+1.5]
+maph_tem = maph[maph.EXPTIME >= mExpTime - 1.5][maph.EXPTIME <= mExpTime + 1.5]
 
-# Filtro con Desviacion estandard
+# Filtro con standard deviation
 stdExpTime = np.std(maph.EXPTIME)
-stdExpTime
-maph_tem = maph[(maph.EXPTIME>=(mExpTime-stdExpTime)) & (maph.EXPTIME<=(mExpTime+stdExpTime))]
+maph_tem = maph[(maph.EXPTIME >= (mExpTime - stdExpTime)) & (maph.EXPTIME <= (mExpTime + stdExpTime))]
 maph_tem.info()
