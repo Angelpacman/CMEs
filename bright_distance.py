0f9e3226c8c3401efafc1d44d8177b25aaedc307
Angel Resendiz <angelr4a1@gmail.com>
2022-05-07 23:54:24 -0500

    Preparar datos de entrada

--- bright_distance.py
+++ bright_distance.py
@@ -15,3 +15,39 @@ from astropy.visualization import (HistEqStretch,
                                    LogStretch)
 from sklearn import preprocessing
 import matplotlib.animation as animation
+from scipy import ndimage, misc
+
+pathToFiles = 'DATA'
+orderedList = sorted(glob.glob(os.path.join(pathToFiles, '*.fts')))
+contador=0
+for filename in orderedList:
+    contador+=1
+print("\nExisten {} archivos fts".format(contador))
+
+
+# Obtener lista de datos que son 2048x2048
+listaDatos = []
+for filename in orderedList:
+  hdulist = fits.open(filename)
+  data = hdulist[0].data
+  hdulist.close
+  if data.shape == (2048, 2048):
+    listaDatos.append(filename)
+print("\nDe los cuales {} son  de tamaño 2048 x 2048".format(len(listaDatos)))
+
+# Obtener headers
+hdulist1 = fits.open(listaDatos[0])
+headerList = hdulist1[0].header
+hdulist1.close()
+maph=pd.DataFrame(columns=headerList)
+
+# Generar mapa de imagenes
+cel = []
+for i in range(len(listaDatos)):
+    hdulist1 = fits.open(listaDatos[i])
+    headerList = hdulist1[0].header
+    hdulist1.close()
+    for headerInplace in headerList:
+        cel.append(headerList[headerInplace])
+    maph.loc[i]=cel
+    cel=[]
